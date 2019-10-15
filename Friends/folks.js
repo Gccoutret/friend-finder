1ebe2ff026d8123ac79605bfe383d08374c08461
G Cout <Gccoutret@gmail.com>
2019-10-14 21:36:24 -0500

    Was using MySQL. Made folder to populate with friends as an API.

---
+++ Friends/folks.js
@@ -0,0 +1,46 @@
+var friendTable = [
+  {
+    name: 'Gordon',
+    image: '../images/self.img.JPG',
+    question1: 3,
+    question2: 3,
+    question3: 3,
+    question4: 3,
+    question5: 3,
+    question6: 3,
+    question7: 3,
+    question8: 2,
+    question9: 2,
+    question10: 2
+  },
+  {
+    name: 'Shawnson',
+    image: 'https://www.pexels.com/photo/adult-beard-boy-casual-220453/',
+    question1: 3,
+    question2: 3,
+    question3: 5,
+    question4: 5,
+    question5: 3,
+    question6: 1,
+    question7: 3,
+    question8: 1,
+    question9: 2,
+    question10: 2
+  },
+  {
+    name: 'Jina',
+    image: 'https://www.pexels.com/photo/closeup-photo-of-woman-with-brown-coat-and-gray-top-733872/',
+    question1: 1,
+    question2: 2,
+    question3: 3,
+    question4: 4,
+    question5: 5,
+    question6: 4,
+    question7: 3,
+    question8: 2,
+    question9: 1,
+    question10: 5,
+  },
+]
+
+module.exports = friendTable;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,20 +1,36 @@
-var connection = require('../connection');
+// var connection = require('../connection');
 
-module.exports=function(app){
-app.get("/api/friends", function(req, res) {
-  res.sendFile(path.join(__dirname, "../public/survey.html"));
-});
+// module.exports=function(app){
+// app.get("/api/friends", function(req, res) {
+//   res.sendFile(path.join(__dirname, "../public/survey.html"));
+// });
 
-app.post("/api/survey", function(req, res) {
-  console.log(req.body)
-  console.log('o we in the api')
-  var query= "INSERT INTO friends (name, photo_link, question1, quesiton2, question3, question4, question5, question6, question7, question8, question9, question10,)VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)"
-  connection.query(query, [req.body.name, req.body.photo_link, req.body.question1, req.body.question2, req.body.question3, req.body.question4, req.body.question5, req.body.question6, req.body.question7, req.body.question8, req.body.question9, req.body.question10], function (error, results) {
-    console.log('o we in the db')
-    if (error) throw error;
-    res.json(results);
-    console.log(req.body)
+// app.post("/api/survey", function(req, res) {
+//   console.log(req.body)
+//   console.log('o we in the api')
+//   var query= "INSERT INTO friends (name, photo_link, question1, quesiton2, question3, question4, question5, question6, question7, question8, question9, question10,)VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)"
+//   connection.query(query, [req.body.name, req.body.photo_link, req.body.question1, req.body.question2, req.body.question3, req.body.question4, req.body.question5, req.body.question6, req.body.question7, req.body.question8, req.body.question9, req.body.question10], function (error, results) {
+//     console.log('o we in the db')
+//     if (error) throw error;
+//     res.json(results);
+//     console.log(req.body)
  
-});
-})
+// });
+// })
+// }
+
+var friendTable = require('../friends/folks');
+
+module.exports = function(app){
+
+  app.get('/api/friends', function(req, res){
+    res.json(allFriends);
+  })
+
+  app.post('/api/friends', function(req, res){
+    console.log('Posting...')
+    friendTable.push(req.body);
+    console.log(allFriends);
+    res.json(true);
+  });
 }
